5c9bf82cd44763d46cb20361c3ce9388d852f358
Guilloux <cecile.guilloux@silecs.info>
2020-10-26 22:06:53 +0100

    version + dépendances

--- version.php
+++ version.php
@@ -9,6 +9,12 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2013021500;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2012061700;        // Requires this Moodle version
-$plugin->component = 'local_mwscoursetree';       // Full name of the plugin (used for diagnostics)
+$plugin->requires  = 2020100300;        // Requires this Moodle version
+$plugin->component = 'local_mwscoursetree';  // Full name of the plugin (used for diagnostics)
+
+$plugin->dependencies = [
+	'local_coursehybridtree' => 2020100300,
+	'local_up1_courselist' => 2020100300,
+	'local_jquery' =>  2020100300
+];
 
